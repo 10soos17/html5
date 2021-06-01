8ad8215984fd1f3d8b4f22c816b43c4f6d795f1a
soos <shinsh1017@gmail.com>
2021-06-01 23:14:12 +0900

    .

--- 2_java/1_2_class/Ch01_Start/src/study14_board/hashMap/BoardMain.java
+++ 2_java/1_2_class/Ch01_Start/src/study14_board/hashMap/BoardMain.java
@@ -6,13 +6,14 @@ public class BoardMain {
 
 	public static void main(String[] args) {
 		boolean isStop = false;
+		boolean isExit = false;
 		Scanner sc = new Scanner(System.in);
 		int select;
 		BoardSVC boardSVC = new BoardSVC();
 
 		do {
 
-			for(int i=0; i<10; i++) {
+			while(!isExit) {
 				try {
 					System.out.println("##메뉴를 입력하세요.##");
 					System.out.println("1.게시판 글쓰기 2.글 목록 보기 3. 글 삭제 4. 종료");
