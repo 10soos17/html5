8e4f2887efda2a014ee310de717e2a2916faaf56
soos <shinsh1017@gmail.com>
2021-06-03 12:46:20 +0900

    0603_am

--- 2_java/1_2_class/Ch17_JavaFX/src/p04/container/AppMain.java
+++ 2_java/1_2_class/Ch17_JavaFX/src/p04/container/AppMain.java
@@ -1,10 +1,27 @@
 package p04.container;
 
-public class AppMain {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppMain extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("root_border.fxml"));
+		
+		Scene s = new Scene(root);
+		
+		stage.setTitle("AppMain");
+		stage.setScene(s);
+		stage.show();
 	}
 
 }
