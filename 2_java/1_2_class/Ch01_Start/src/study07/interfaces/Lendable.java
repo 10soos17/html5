494a4b51b0010b611ea6dd0086d2c455a42dfd86
10soos17 <44848864+10soos17@users.noreply.github.com>
2021-06-05 22:31:36 +0900

    Delete 2_java/1_2_class/Ch01_Start directory
    delete

--- 2_java/1_2_class/Ch01_Start/src/study07/interfaces/Lendable.java
+++
@@ -1,9 +0,0 @@
-package study07.interfaces;
-
-public interface Lendable {
-	//대출
-	public abstract void checkOut(String borrower, String checkOutDate);
-	//반납
-	public abstract void checkIn();
-	
-}
